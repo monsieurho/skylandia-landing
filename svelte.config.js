044d34d89ecbe7e1c7a9a0a7b1244a2ca7da56c1
monsieurho <87746215+monsieurho@users.noreply.github.com>
2022-03-10 20:42:23 +0800

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,6 @@ import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		adapter: adapter(),
-		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
 		}
